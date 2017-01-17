564389db94f6255ae7b050a491db996aa6f61e48
somaakihiro <aki.air@icloud.com>
2017-01-18 00:47:19 +0900

    ルーティングの基礎

--- myapp.rb
+++ myapp.rb
@@ -11,3 +11,23 @@ end
 get '/hello/:name' do |n|
   "Hello #{n}"
 end
+
+get '/say/*/to/*' do
+  params['splat']
+end
+
+get '/download/*.*' do
+  params['splat']
+end
+
+get '/downloads/*.*' do |path, ext|
+  [path, ext]
+end
+
+get %r{/hello/([\w]+)} do |c|
+  "Hello, #{c}"
+end
+
+get 'posts.?:format?' do
+  '"GET /posts" と "GET /posts.json", "GET /posts.xml" の拡張子などにマッチ'
+end
