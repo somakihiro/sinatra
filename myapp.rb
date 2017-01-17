f89423aafa29cf7a6ad35ccca013a5be25a69117
somaakihiro <aki.air@icloud.com>
2017-01-18 00:32:58 +0900

    ブロックパラメータ

--- myapp.rb
+++ myapp.rb
@@ -4,6 +4,10 @@ get '/' do
   'Hello world!'
 end
 
-get '/hello/:name' do
-  "Hello #{params['name']}!"
+# get '/hello/:name' do
+#   "Hello #{params['name']}!"
+# end
+
+get '/hello/:name' do |n|
+  "Hello #{n}"
 end
