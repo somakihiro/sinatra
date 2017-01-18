781492023bf284e540a8e63bd7c300227c49d0ac
somaakihiro <aki.air@icloud.com>
2017-01-18 20:11:09 +0900

    コメント一覧表示の準備

--- myapp.rb
+++ myapp.rb
@@ -1,57 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-before do
-  @author = 'somakihiro'
-end
-helpers do
-  def link(msg)
-    "hello, #{msg}"
-  end
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
 end
 
 get '/' do
-  @title = 'main'
-  @msg = link(@author)
+  @comments = Comment.order("id desc").all
   haml :index
-end
-
-# get '/hello/:name' do
-#   "Hello #{params['name']}!"
-# end
-
-get '/hello/:name' do |n|
-  "Hello #{n}"
-end
-
-get '/say/*/to/*' do
-  params['splat']
-end
-
-get '/download/*.*' do
-  params['splat']
-end
-
-get '/downloads/*.*' do |path, ext|
-  [path, ext]
-end
-
-get %r{/hello/([\w]+)} do |c|
-  "Hello, #{c}"
-end
-
-get 'posts.?:format?' do
-  '"GET /posts" と "GET /posts.json", "GET /posts.xml" の拡張子などにマッチ'
-end
-
-get '/', :host_name => /^admin\./ do
-  "Adminエリア"
-end
-
-get '/', :provides => 'html' do
-  haml :index
-end
-
-get '/', :provides => ['rss', 'atom', 'xml'] do
-  builder :feed
 end
